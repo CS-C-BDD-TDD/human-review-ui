efdd66fd62b966604cb3d257593ca81e6190d352
Deven Phillips <deven.phillips@gmail.com>
2018-10-19 15:02:11 -0400

    Revised older tests to comply with new code

--- tests/unit/steps/login.steps.js
+++ tests/unit/steps/login.steps.js
@@ -60,11 +60,114 @@ defineFeature(feature, test => {
         expect(wrapper.vm.$data.password).toBeDefined();
         expect(wrapper.vm.$data.password).toBe(PASSWORD);
     });
+  });
 
-    then(/^the system sends the credentials$/, () => {
-      expect(wrapper.emitted("login")).toBeDefined();
-      expect(wrapper.emitted("login").length).toEqual(1);
-      expect(wrapper.emitted("login")[0][0]).toEqual(EXPECTED_CREDENTIALS);
+  test('Verify login logic works in LogIn component', ({ given, when, then }) => {
+    let wrapper;
+    const $api = {};
+    const $router = {};
+
+    given('a mock instance of the API client', () => {
+      /**
+       * Mock implementation of the `userPut` method for the API client library
+       */
+      $api.userPut = jest.fn((credentials, callback) => {
+        expect(credentials.username).toEqual('username');
+        expect(credentials.password).toEqual('password');
+        callback(null, 'example_token', {});
+      });
+    });
+
+    given('a mock instance of the Vue router', () => {
+      /**
+       * Mock implementation of the `$router.push` method
+       */
+      $router.push = jest.fn((pushInfo) => {
+        expect(pushInfo.name).toEqual('humanreview');
+        expect(pushInfo.params).toEqual({ token: 'example_token' });
+      });
+    });
+
+    given('an instance of the LogIn component with our mocks injected', () => {
+      wrapper = mount(LogIn, {
+        localVue,
+        mocks: { // Implement the mocks here!
+          $api,
+          $router
+        }
+      });
+    });
+
+    when('I enter a valid username', () => {
+      wrapper.find('input[type=text]').setValue('username');
+    });
+
+    when('I enter a valid password', () => {
+      wrapper.find('input[type=password]').setValue('password');
+    });
+
+    when('I click the LOGIN button', () => {
+      wrapper.find('button').trigger('click');
+    });
+
+    then('I expect that the REST API Client will be called with appropriate parameters', async () => {
+      await wrapper.vm.$nextTick();
+      expect(wrapper.vm.$data.token).toEqual('example_token');
+      expect($api.userPut).toHaveBeenCalled();
+    });
+
+    then('I expect that the user will have been navigated to the HumanReview page', () => {
+      expect($router.push).toHaveBeenCalled();
+    });
+
+    then('I expect that the failed login alert is not visible', () => {
+      expect(wrapper.vm.$data.failedLogin).toBeFalsy();
+    });
+  });
+
+  test('Verify login logic handles invalid username properly', ({ given, when, then }) => {
+    let wrapper;
+    const $api = {};
+
+    given('a mock instance of the API client', () => {
+      /**
+       * Mock implementation of the `userPut` method for the API client library
+       */
+      $api.userPut = jest.fn((credentials, callback) => {
+        expect(credentials.username).toEqual('username');
+        expect(credentials.password).toEqual('password');
+        callback('Invalid username and/or password', null, {});
+      });
+    });
+
+    given('an instance of the LogIn component with our mock injected', () => {
+      wrapper = mount(LogIn, {
+        localVue,
+        mocks: { // Implement the mocks here!
+          $api
+        }
+      });
+    });
+
+    when('I enter an invalid username', () => {
+      wrapper.find('input[type=text]').setValue('username');
+    });
+
+    when('I enter a password', () => {
+      wrapper.find('input[type=password]').setValue('password');
+    });
+
+    when('I click the LOGIN button', () => {
+      wrapper.find('button').trigger('click');
+    });
+
+    then('I expect that the REST API Client will be called with appropriate parameters', async () => {
+      await wrapper.vm.$nextTick();
+      expect($api.userPut).toHaveBeenCalled();
+    });
+
+    then('I expect an error message to be displayed on the Login screen', () => {
+      expect(wrapper.vm.$data.failedLogin).toBeTruthy();
     });
   });
 });
