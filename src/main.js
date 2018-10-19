8c3132e17b96601968d9000012a67d835643adf4
Deven Phillips <deven.phillips@gmail.com>
2018-10-19 14:59:19 -0400

    Fix style problem

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+import HumanReview from 'human_review';
 import Vue from 'vue';
 import Quasar from 'quasar';
 import iconSet from 'quasar-framework/icons/fontawesome';
@@ -6,7 +7,6 @@ import './registerServiceWorker';
 import './styles/quasar.styl';
 import App from './App.vue';
 import router from './router';
-import HumanReview from 'human_review';
 
 Vue.prototype.$api = new HumanReview.ApiClient();
 Vue.prototype.$api.basePath = 'https://humanreview-labs-dev.apps.domino.rht-labs.com/api/v1';
