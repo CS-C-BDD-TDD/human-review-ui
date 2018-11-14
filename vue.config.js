b5721517d646dc0e5df9a5ff46491bfd6be25415
Kiet Tran <drkiettran@gmail.com>
2018-11-14 05:05:12 -0500

    Changed hardcoded backend url to test on a different project

--- vue.config.js
+++ vue.config.js
@@ -9,12 +9,12 @@ module.exports = {
   devServer: {
     proxy: {
       '/api/v1/user': {
-        target: 'http://human-review-backend-labs-test.apps.domino.rht-labs.com',
+        target: 'http://human-review-backend-labs-devdemo.apps.domino.rht-labs.com/',
         //target: 'http://localhost:8080',
         changeOrigin: true,
       },
       '/api/v1/humanreview/pending': {
-        target: 'http://human-review-backend-labs-test.apps.domino.rht-labs.com',
+        target: 'http://human-review-backend-labs-devdemo.apps.domino.rht-labs.com/',
         //target: 'http://localhost:8080',
         changeOrigin: true,
       },
